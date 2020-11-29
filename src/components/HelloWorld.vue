46616c29ea81c3990f2a0de8c850dd858083c329
Mirzet Kameric <mirzetkameric@hotmail.com>
2020-11-29 11:52:43 +0100

    added initial layout for select of locations

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="text-center">
     <h1>{{ msg }}</h1>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,57 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <hello-world class="mb-3" msg="Choose where you want to live"/>
+        <form>
+            <div class="row">
+                <div class="col-5">
+                    <div class="form-group">
+                        <label for="location">Where do you live?</label>
+                        <select class="form-control" v-model="locationFrom" name="location" id="location" required>
+                            <option v-for="location in locations" :key="location" :value="location">{{ location }}</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-5">
+                    <div class="form-group">
+                        <label for="locationTo">Where do you want to live?</label>
+                        <select class="form-control" v-model="locationTo" name="locationTo" id="locationTo" required>
+                            <option v-for="location in locations" :key="location" :value="location">{{ location }}</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <button type="submit" class="btn btn-dark btn-explore">Explore</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    components: {
+        HelloWorld
+    },
+    data () {
+        return {
+            locations: [
+                'Amsterdam',
+                'Madrid',
+                'Budapest'
+            ],
+            locationFrom: null,
+            locationTo: null
+        }
+    }
 }
 </script>
+
+<style lang="scss" scoped>
+    .btn-explore {
+        margin-top: 36px;
+        width: 100%;
+    }
+</style>
